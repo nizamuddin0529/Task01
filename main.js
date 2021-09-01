0229e02e09c019ade9d584aa20d6b507ff93c5b0
nizamuddin0529 <89502422+nizamuddin0529@users.noreply.github.com>
2021-09-01 11:58:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Mozilla is cool! Nizamuddin';
+/*function setUserName() {
+    let myName = prompt('please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else{
+      localStorage.setItem('name',SK);
+      myHeading.textContent = 'Mozilla is cool,' + myname;
+    }
+ }*/
